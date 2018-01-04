1bd95c2fab078a64378073a90513fb31e2cb98ce
Jaakko Ojalehto <jaakko@ojalehto.fi>
2018-01-04 21:20:24 +0200

    Add eslint and airbnb lint rules

--- server/src/index.js
+++ server/src/index.js
@@ -6,4 +6,4 @@ const app = express()
 app.get('/', (req, res) => res.sendFile('client/src/html/index.html', { root: path.resolve(__dirname, '../../') }))
 app.get('/client.js', (req, res) => res.sendFile('client/dist/project-rally-point-client.js', { root: path.resolve(__dirname, '../../') }))
 
-app.listen(8080, () => console.log('Project Rally Point listening on port 8080!'))
+app.listen(8080, () => console.log('Project Rally Point listening on port 8080!')) // eslint-disable-line no-console
