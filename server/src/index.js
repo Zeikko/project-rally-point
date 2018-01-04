90dc73b21ab4899d4482f89db2cfafc2d33025d8
Jaakko Ojalehto <jaakko@ojalehto.fi>
2018-01-04 22:06:29 +0200

    Fix clietn.js filename

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,6 @@ import path from 'path'
 const app = express()
 
 app.get('/', (req, res) => res.sendFile('client/src/html/index.html', { root: path.resolve(__dirname, '../../') }))
-app.get('/client.js', (req, res) => res.sendFile('client/dist/project-rally-point-client.js', { root: path.resolve(__dirname, '../../') }))
+app.get('/client.js', (req, res) => res.sendFile('client/dist/index.js', { root: path.resolve(__dirname, '../../') }))
 
 app.listen(8080, () => console.log('Project Rally Point listening on port 8080!')) // eslint-disable-line no-console
