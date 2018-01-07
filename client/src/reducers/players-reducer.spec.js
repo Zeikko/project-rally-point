4e025ccb90a6e0615be28b44b86339148cb2b9fc
Jaakko Ojalehto <jaakko@ojalehto.fi>
2018-01-07 16:05:30 +0200

    Add reducer tests

---
+++ client/src/reducers/players-reducer.spec.js
@@ -0,0 +1,21 @@
+import playersReducer from './players-reducer'
+import actions from '../../../common/actions.json'
+import normalUser from '../../../fixtures/normal-user.json'
+import initialState from '../initial-state'
+
+describe('playersReducer', () => {
+  it('returns old state', () => {
+    const nextState = playersReducer(undefined, {
+      type: 'irrelevant action',
+    })
+    expect(nextState).toEqual(initialState.players)
+  })
+
+  it('updates players', () => {
+    const nextState = playersReducer(undefined, {
+      type: actions.GET_PLAYERS_SUCCESS,
+      data: [normalUser],
+    })
+    expect(nextState).toEqual({ data: [normalUser] })
+  })
+})
